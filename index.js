f1942ae35557a34960fb4e69cc6c2260254b8fbc
Yamitzky <negiga@gmail.com>
2018-05-07 20:06:21 +0900

    Refactoring table and dashboard feature

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const tempfile = require("tempfile")
 const fs = require("fs")
 const request = require('request-promise-native')
 const sleep = require('await-sleep')
+const Table = require('table-layout')
 
 
 // This configuration can gets overwritten when process.env.SLACK_MESSAGE_EVENTS is given.
@@ -54,85 +55,101 @@ controller.spawn({
   token: slackBotToken
 }).startRTM()
 
-Object.keys(redashApiKeysPerHost).forEach((redashHost) => {
-  const redashHostAlias = redashApiKeysPerHost[redashHost]["alias"]
-  const redashApiKey    = redashApiKeysPerHost[redashHost]["key"]
-  controller.hears(`${redashHost}/(queries/([0-9]+)#([0-9]+|table)|public/dashboards/([a-zA-Z0-9]+))`, slackMessageEvents, async (bot, message) => {
-    const originalUrl = message.match[0]
-    // for embed
-    const queryId = message.match[2]
-    const visualizationId =  message.match[3]
-    // for dashboard
-    const dashboardId = message.match[4]
-
-    let query = null
-    let visualization = null
-    let visualizationPrimaryKey = visualizationId
-    if (queryId) {
-      try {
-        const body = await request.get({ uri: `${redashHost}/api/queries/${queryId}`, qs: { api_key: redashApiKey } })
-        query = JSON.parse(body)
-        visualization = query.visualizations.find(vis => vis.id.toString() === visualizationId || vis.type.toLowerCase() === visualizationId)
-        visualizationPrimaryKey = visualization.id
-      } catch (err) {
-        bot.botkit.log.error(err)
-      }
-    }
-
-    let queryUrl = `${redashHostAlias}/queries/${queryId}#${visualizationId}`
-    let embedUrl = `${redashHostAlias}/embed/query/${queryId}/visualization/${visualizationPrimaryKey}?api_key=${redashApiKey}`
-    let filename = `query-${queryId}-visualization-${visualizationId}.png`
-    if (query && visualization) {
-      filename = `${query.name}-${visualization.name}-${filename}`
-    }
-    if (dashboardId) {
-      queryUrl = `${redashHostAlias}/public/dashboards/${dashboardId}`
-      embedUrl = queryUrl
-      filename = `dashboard-${dashboardId}.png`
-    }
-
-    bot.reply(message, `Taking screenshot of ${originalUrl}`)
-    bot.botkit.log(queryUrl)
-    bot.botkit.log(embedUrl)
-
-    const outputFile = tempfile(".png")
-    const webshotOptions = {
-      shotSize: {
-        width: 720,
-        height: "all"
-      },
-    }
-
+const faultTolerantMiddleware = (func) => {
+  return async (bot, message) => {
     try {
-      const browser = await puppeteer.launch({
-        executablePath: process.env.CHROMIUM_BROWSER_PATH,
-        args: ['--disable-dev-shm-usage', '--no-sandbox']
-      })
-      const page = await browser.newPage()
-      page.setViewport({ width: 720, height: 360 })
-      await page.goto(embedUrl)
-      await sleep(2000)
-      await page.screenshot({ path: outputFile, fullPage: true })
-      await browser.close()
-
-      bot.botkit.log.debug(outputFile)
-      bot.botkit.log.debug(Object.keys(message))
-      bot.botkit.log(message.user + ":" + message.type + ":" + message.channel + ":" + message.text)
-
-      const options = {
-        token: slackBotToken,
-        filename: filename,
-        file: fs.createReadStream(outputFile),
-        channels: message.channel
-      }
-
-      // bot.api.file.upload cannot upload binary file correctly, so directly call Slack API.
-      const body = await request.post({ url: "https://api.slack.com/api/files.upload", formData: options, simple: true })
+      await func(bot, message)
       bot.botkit.log("ok")
     } catch (err) {
-      const msg = `Something wrong happend in take a screen capture : ${err}`
+      const msg = `Something wrong happend : ${err}`
       bot.reply(message, msg)
       bot.botkit.log.error(msg)
     }
+  }
+}
+
+const takeScreenshot = async (url) => {
+  const file = tempfile(".png")
+  const browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_BROWSER_PATH,
+    args: ['--disable-dev-shm-usage', '--no-sandbox']
   })
+  const page = await browser.newPage()
+  page.setViewport({ width: 1024, height: 360 })
+  await page.goto(url)
+  await sleep(2000)
+  await page.screenshot({ path: file, fullPage: true })
+  await browser.close()
+  return file
+}
+
+const uploadFile = async (channel, filename, file) => {
+  const options = {
+    token: slackBotToken,
+    filename: filename,
+    file: fs.createReadStream(file),
+    channels: channel
+  }
+  await request.post({ url: "https://api.slack.com/api/files.upload", formData: options, simple: true })
+}
+
+Object.keys(redashApiKeysPerHost).forEach((redashHost) => {
+  const redashHostAlias = redashApiKeysPerHost[redashHost]["alias"]
+  const redashApiKey    = redashApiKeysPerHost[redashHost]["key"]
+
+  controller.hears(`${redashHost}/queries/([0-9]+)#([0-9]+)`, slackMessageEvents, faultTolerantMiddleware(async (bot, message) => {
+    const [ originalUrl, queryId, visualizationId ] = message.match
+
+    const body = await request.get({ uri: `${redashHost}/api/queries/${queryId}`, qs: { api_key: redashApiKey }, simple: true })
+    const query = JSON.parse(body)
+    const visualization = query.visualizations.find(vis => vis.id.toString() === visualizationId)
+
+    const embedUrl = `${redashHostAlias}/embed/query/${queryId}/visualization/${visualizationId}?api_key=${redashApiKey}`
+    const filename = `${query.name}-${visualization.name}-query-${queryId}-visualization-${visualizationId}.png`
+
+    bot.reply(message, `Taking screenshot of ${originalUrl}`)
+    bot.botkit.log(embedUrl)
+    const output = await takeScreenshot(embedUrl)
+    uploadFile(message.channel, filename, output)
+  }))
+
+  controller.hears(`${redashHost}/dashboard/([^?/|>]+)`, slackMessageEvents, faultTolerantMiddleware(async (bot, message) => {
+    const [ originalUrl, dashboardId ] = message.match
+
+    const body = await request.get({ uri: `${redashHost}/api/dashboards/${dashboardId}`, qs: { api_key: redashApiKey }, simple: true })
+    const dashboard = JSON.parse(body)
+
+    const filename = `${dashboard.name}-dashboard-${dashboardId}.png`
+
+    bot.reply(message, `Taking screenshot of ${originalUrl}`)
+    bot.botkit.log(dashboard.public_url)
+    const output = await takeScreenshot(dashboard.public_url)
+    uploadFile(message.channel, filename, output)
+  }))
+
+  controller.hears(`${redashHost}/queries/([0-9]+)(?:#table)?`, slackMessageEvents, faultTolerantMiddleware(async (bot, message) => {
+    const [ originalUrl, queryId ] = message.match
+    const body = await request.get({ uri: `${redashHost}/api/queries/${queryId}`, qs: { api_key: redashApiKey }, simple: true })
+    const query = JSON.parse(body)
+
+    const result = JSON.parse(await request.get({ uri: `${redashHost}/api/queries/${queryId}/results.json`, qs: { api_key: redashApiKey }, simple: true })).query_result.data
+
+    const rows = result.rows.map(row => {
+      const converted = {}
+      for (const { friendly_name, name } of result.columns) {
+        converted[friendly_name] = row[name]
+      }
+      return converted
+    })
+
+    const cols = {}
+    for (const { friendly_name } of result.columns) {
+      const dashes = '-'.repeat(friendly_name.length)
+      cols[friendly_name] = `${friendly_name}\n${dashes}`
+    }
+    const table = new Table([cols].concat(rows))
+    let tableMessage = '```' + table.toString() + '```'
+    tableMessage = tableMessage.split('\n').map(line => line.trimRight()).join('\n')
+    bot.reply(message, `${query.name}\n${tableMessage}`)
+  }))
 })
