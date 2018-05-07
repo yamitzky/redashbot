aae858e4f88a53bd36550eca1c3decd0eb2eac15
Yamitzky <negiga@gmail.com>
2018-05-07 13:22:17 +0900

    Semicolonless JS

--- index.js
+++ index.js
@@ -1,101 +1,101 @@
-"use strict";
+"use strict"
 
-const Botkit = require("botkit");
-const webshot = require("webshot");
-const tempfile = require("tempfile");
-const fs = require("fs");
-const request = require("request");
-const rp = require('request-promise-native');
+const Botkit = require("botkit")
+const webshot = require("webshot")
+const tempfile = require("tempfile")
+const fs = require("fs")
+const request = require("request")
+const rp = require('request-promise-native')
 
 
 // This configuration can gets overwritten when process.env.SLACK_MESSAGE_EVENTS is given.
-const DEFAULT_SLACK_MESSAGE_EVENTS = "direct_message,direct_mention,mention";
+const DEFAULT_SLACK_MESSAGE_EVENTS = "direct_message,direct_mention,mention"
 
 if (!process.env.SLACK_BOT_TOKEN) {
-  console.error("Error: Specify SLACK_BOT_TOKEN in environment values");
-  process.exit(1);
+  console.error("Error: Specify SLACK_BOT_TOKEN in environment values")
+  process.exit(1)
 }
 if (!((process.env.REDASH_HOST && process.env.REDASH_API_KEY) || (process.env.REDASH_HOSTS_AND_API_KEYS))) {
-  console.error("Error: Specify REDASH_HOST and REDASH_API_KEY in environment values");
-  console.error("Or you can set multiple Re:dash configs by specifying like below");
-  console.error("REDASH_HOSTS_AND_API_KEYS=\"http://redash1.example.com;TOKEN1,http://redash2.example.com;TOKEN2\"");
-  process.exit(1);
+  console.error("Error: Specify REDASH_HOST and REDASH_API_KEY in environment values")
+  console.error("Or you can set multiple Re:dash configs by specifying like below")
+  console.error("REDASH_HOSTS_AND_API_KEYS=\"http://redash1.example.com;TOKEN1,http://redash2.example.com;TOKEN2\"")
+  process.exit(1)
 }
 
 const parseApiKeysPerHost = () => {
   if (process.env.REDASH_HOST) {
     if (process.env.REDASH_HOST_ALIAS) {
-      return {[process.env.REDASH_HOST]: {"alias": process.env.REDASH_HOST_ALIAS, "key": process.env.REDASH_API_KEY}};
+      return {[process.env.REDASH_HOST]: {"alias": process.env.REDASH_HOST_ALIAS, "key": process.env.REDASH_API_KEY}}
     } else {
-      return {[process.env.REDASH_HOST]: {"alias": process.env.REDASH_HOST, "key": process.env.REDASH_API_KEY}};
+      return {[process.env.REDASH_HOST]: {"alias": process.env.REDASH_HOST, "key": process.env.REDASH_API_KEY}}
     }
   } else {
     return process.env.REDASH_HOSTS_AND_API_KEYS.split(",").reduce((m, host_and_key) => {
-      var [host, alias, key] = host_and_key.split(";");
+      var [host, alias, key] = host_and_key.split(";")
       if (!key) {
-        key = alias;
-        alias = host;
+        key = alias
+        alias = host
       }
-      m[host] = {"alias": alias, "key": key};
-      return m;
-    }, {});
+      m[host] = {"alias": alias, "key": key}
+      return m
+    }, {})
   }
-};
+}
 
-const redashApiKeysPerHost = parseApiKeysPerHost();
-const slackBotToken = process.env.SLACK_BOT_TOKEN;
-const slackMessageEvents = process.env.SLACK_MESSAGE_EVENTS || DEFAULT_SLACK_MESSAGE_EVENTS;
+const redashApiKeysPerHost = parseApiKeysPerHost()
+const slackBotToken = process.env.SLACK_BOT_TOKEN
+const slackMessageEvents = process.env.SLACK_MESSAGE_EVENTS || DEFAULT_SLACK_MESSAGE_EVENTS
 
 const controller = Botkit.slackbot({
   debug: !!process.env.DEBUG
-});
+})
 
 controller.spawn({
   token: slackBotToken
-}).startRTM();
+}).startRTM()
 
 Object.keys(redashApiKeysPerHost).forEach((redashHost) => {
-  const redashHostAlias = redashApiKeysPerHost[redashHost]["alias"];
-  const redashApiKey    = redashApiKeysPerHost[redashHost]["key"];
+  const redashHostAlias = redashApiKeysPerHost[redashHost]["alias"]
+  const redashApiKey    = redashApiKeysPerHost[redashHost]["key"]
   controller.hears(`${redashHost}/(queries/([0-9]+)#([0-9]+|table)|public/dashboards/([a-zA-Z0-9]+))`, slackMessageEvents, async (bot, message) => {
-    const originalUrl = message.match[0];
+    const originalUrl = message.match[0]
     // for embed
-    const queryId = message.match[2];
-    const visualizationId =  message.match[3];
+    const queryId = message.match[2]
+    const visualizationId =  message.match[3]
     // for dashboard
-    const dashboardId = message.match[4];
+    const dashboardId = message.match[4]
 
-    let query = null;
-    let visualization = null;
-    let visualizationPrimaryKey = visualizationId;
+    let query = null
+    let visualization = null
+    let visualizationPrimaryKey = visualizationId
     if (queryId) {
       try {
-        const body = await rp.get({ uri: `${redashHost}/api/queries/${queryId}`, qs: { api_key: redashApiKey } });
-        query = JSON.parse(body);
-        visualization = query.visualizations.find(vis => vis.id.toString() === visualizationId || vis.type.toLowerCase() === visualizationId);
-        visualizationPrimaryKey = visualization.id;
+        const body = await rp.get({ uri: `${redashHost}/api/queries/${queryId}`, qs: { api_key: redashApiKey } })
+        query = JSON.parse(body)
+        visualization = query.visualizations.find(vis => vis.id.toString() === visualizationId || vis.type.toLowerCase() === visualizationId)
+        visualizationPrimaryKey = visualization.id
       } catch (err) {
-        bot.botkit.log.error(err);
+        bot.botkit.log.error(err)
       }
     }
 
-    let queryUrl = `${redashHostAlias}/queries/${queryId}#${visualizationId}`;
-    let embedUrl = `${redashHostAlias}/embed/query/${queryId}/visualization/${visualizationPrimaryKey}?api_key=${redashApiKey}`;
-    let filename = `query-${queryId}-visualization-${visualizationId}.png`;
+    let queryUrl = `${redashHostAlias}/queries/${queryId}#${visualizationId}`
+    let embedUrl = `${redashHostAlias}/embed/query/${queryId}/visualization/${visualizationPrimaryKey}?api_key=${redashApiKey}`
+    let filename = `query-${queryId}-visualization-${visualizationId}.png`
     if (query && visualization) {
       filename = `${query.name}-${visualization.name}-${filename}`
     }
     if (dashboardId) {
-      queryUrl = `${redashHostAlias}/public/dashboards/${dashboardId}`;
-      embedUrl = queryUrl;
-      filename = `dashboard-${dashboardId}.png`;
+      queryUrl = `${redashHostAlias}/public/dashboards/${dashboardId}`
+      embedUrl = queryUrl
+      filename = `dashboard-${dashboardId}.png`
     }
 
-    bot.reply(message, `Taking screenshot of ${originalUrl}`);
-    bot.botkit.log(queryUrl);
-    bot.botkit.log(embedUrl);
+    bot.reply(message, `Taking screenshot of ${originalUrl}`)
+    bot.botkit.log(queryUrl)
+    bot.botkit.log(embedUrl)
 
-    const outputFile = tempfile(".png");
+    const outputFile = tempfile(".png")
     const webshotOptions = {
       screenSize: {
         width: 720,
@@ -107,40 +107,40 @@ Object.keys(redashApiKeysPerHost).forEach((redashHost) => {
       },
       renderDelay: 2000,
       timeout: 100000
-    };
+    }
 
     webshot(embedUrl, outputFile, webshotOptions, (err) => {
       if (err) {
-        const msg = `Something wrong happend in take a screen capture : ${err}`;
-        bot.reply(message, msg);
-        return bot.botkit.log.error(msg);
+        const msg = `Something wrong happend in take a screen capture : ${err}`
+        bot.reply(message, msg)
+        return bot.botkit.log.error(msg)
       }
 
-      bot.botkit.log.debug(outputFile);
-      bot.botkit.log.debug(Object.keys(message));
-      bot.botkit.log(message.user + ":" + message.type + ":" + message.channel + ":" + message.text);
+      bot.botkit.log.debug(outputFile)
+      bot.botkit.log.debug(Object.keys(message))
+      bot.botkit.log(message.user + ":" + message.type + ":" + message.channel + ":" + message.text)
 
       const options = {
         token: slackBotToken,
         filename: filename,
         file: fs.createReadStream(outputFile),
         channels: message.channel
-      };
+      }
 
       // bot.api.file.upload cannot upload binary file correctly, so directly call Slack API.
       request.post({ url: "https://api.slack.com/api/files.upload", formData: options }, (err, resp, body) => {
         if (err) {
-          const msg = `Something wrong happend in file upload : ${err}`;
-          bot.reply(message, msg);
-          bot.botkit.log.error(msg);
+          const msg = `Something wrong happend in file upload : ${err}`
+          bot.reply(message, msg)
+          bot.botkit.log.error(msg)
         } else if (resp.statusCode == 200) {
-          bot.botkit.log("ok");
+          bot.botkit.log("ok")
         } else {
-          const msg = `Something wrong happend in file upload : status code=${resp.statusCode}`;
-          bot.reply(message, msg);
-          bot.botkit.log.error(msg);
+          const msg = `Something wrong happend in file upload : status code=${resp.statusCode}`
+          bot.reply(message, msg)
+          bot.botkit.log.error(msg)
         }
-      });
-    });
-  });
-});
+      })
+    })
+  })
+})
